6d7208e201167eee3afca3837ed684d6dcea1784
Vibol Teav <vibolteav@gmail.com>
2020-05-12 14:51:57 +0700

    updated deployment config

---
+++ config/deploy.rb
@@ -0,0 +1,13 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.14.0"
+
+set :application, 'oscar-master-dashboard'
+set :repo_url, "git@github.com:rotati/#{fetch(:application)}.git"
+set :deploy_to, "/var/www/#{fetch(:application)}"
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', "public/packs", ".bundle", "node_modules")
+set :linked_files, fetch(:linked_files, []).push('.env')
+
+set :rvm_ruby_version, '2.7.0@oscar-md'
+set :pty, false
+set :keep_releases, 5
+set :passenger_restart_with_touch, true
