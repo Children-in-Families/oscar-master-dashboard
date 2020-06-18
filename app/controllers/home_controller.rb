dbc53e5db27eb882bf24c8e1d66e3f8cc3790949
Vibol T <vibolteav@gmail.com>
2020-06-18 16:52:57 +0700

    updated routes

--- app/controllers/home_controller.rb
+++
@@ -1,6 +0,0 @@
-class HomeController < ApplicationController
-  # def test
-  #   response = HTTParty.post("http://localhost:3000/api/v1/organizations", headers: { Authorization: "Token token=#{current_admin_user&.token}" })
-  #   pp response
-  # end
-end
