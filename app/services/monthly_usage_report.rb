78ad0241a787cb0e012945be40f90049808db1bc
Vibol T <vibolteav@gmail.com>
2020-06-22 12:53:39 +0700

    refactor report

--- app/services/monthly_usage_report.rb
+++ app/services/monthly_usage_report.rb
@@ -1,24 +1,20 @@
 class MonthlyUsageReport
   attr_reader :end_of_month, :beginning_of_month, :organization
 
-  def initialize(organization)
+  def initialize(organization = nil)
     @organization = organization
     @beginning_of_month = 1.month.ago.beginning_of_month
     @end_of_month       = 1.month.ago.end_of_month
   end
 
   def data
-    Organization.switch_to(organization.short_name)
+    Organization.switch_to(organization.short_name) if @organization
 
     previous_month_users = PaperTrail::Version.where(item_type: 'User', created_at: beginning_of_month..end_of_month)
     previous_month_clients = PaperTrail::Version.where(item_type: 'Client', created_at: beginning_of_month..end_of_month)
     tranferred_clients = PaperTrail::Version.where(item_type: 'Referral', event: 'create', created_at: beginning_of_month..end_of_month)
 
-    {
-      ngo_name: organization.full_name,
-      ngo_on_board: organization.created_at.strftime("%d %B %Y"),
-      fcf: organization.fcf_ngo? ? 'Yes' : 'No',
-      ngo_country: Setting.first&.country_name&.titleize,
+    output = {
       user_count: User.non_devs.count,
       user_added_count: previous_month_users.where(event: 'create').count,
       user_deleted_count: previous_month_users.where(event: 'destroy').count,
@@ -28,5 +24,16 @@ class MonthlyUsageReport
       client_deleted_count: previous_month_clients.where(event: 'destroy').count,
       tranferred_client_count: tranferred_clients.map{ |a| a.changeset.dig(:slug) && a.changeset[:slug][1] }.compact.uniq.count
     }
+
+    if @organization
+      output.merge(
+        ngo_name: organization.full_name,
+        ngo_on_board: organization.created_at.strftime("%d %B %Y"),
+        fcf: organization.fcf_ngo? ? 'Yes' : 'No',
+        ngo_country: Setting.first&.country_name&.titleize,
+      )
+    end
+
+    output
   end
 end
