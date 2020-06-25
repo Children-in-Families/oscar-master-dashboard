815463c97cdb858f8482a84307d802df67af332f
Vibol T <vibolteav@gmail.com>
2020-06-25 14:48:39 +0700

    updated report logic

--- app/services/monthly_usage_report.rb
+++ app/services/monthly_usage_report.rb
@@ -9,25 +9,25 @@ class MonthlyUsageReport
   def aggregate_data_without_dev
     output = {
       total_instance: 0,
-      user_count_all_time: 0,
+      user_count: 0,
       user_added_count: 0,
       user_deleted_count: 0,
       login_per_month: 0,
-      client_count_all_time: 0,
+      client_count: 0,
       client_added_count: 0,
       client_deleted_count: 0,
       tranferred_client_count: 0
     }
 
-    Organization.non_demo.find_each do |organization|
+    Organization.non_demo.where(created_at: beginning_of_month..end_of_month).find_each do |organization|
       data = data_per_org(organization)
 
       output[:total_instance] += 1
-      output[:user_count_all_time] += data[:user_count_all_time]
+      output[:user_count] += data[:user_count]
       output[:user_added_count] += data[:user_added_count]
       output[:user_deleted_count] += data[:user_deleted_count]
       output[:login_per_month] += data[:login_per_month]
-      output[:client_count_all_time] += data[:client_count_all_time]
+      output[:client_count] += data[:client_count]
       output[:client_added_count] += data[:client_added_count]
       output[:client_deleted_count] += data[:client_deleted_count]
       output[:tranferred_client_count] += data[:tranferred_client_count]
@@ -47,11 +47,11 @@ class MonthlyUsageReport
         ngo_on_board: organization.created_at.strftime("%d %B %Y"),
         fcf: organization.fcf_ngo? ? 'Yes' : 'No',
         ngo_country: Setting.first&.country_name&.titleize,
-        user_count_all_time: User.non_devs.count,
+        user_count: User.non_devs.where(created_at: beginning_of_month..end_of_month).count,
         user_added_count: previous_month_users.where(event: 'create').count,
         user_deleted_count: previous_month_users.where(event: 'destroy').count,
         login_per_month: Visit.excludes_non_devs.total_logins(beginning_of_month, end_of_month).count,
-        client_count_all_time: Client.count,
+        client_count: Client.where(created_at: beginning_of_month..end_of_month).count,
         client_added_count: previous_month_clients.where(event: 'create').count,
         client_deleted_count: previous_month_clients.where(event: 'destroy').count,
         tranferred_client_count: tranferred_clients.map{ |a| a.changeset.dig(:slug) && a.changeset[:slug][1] }.compact.uniq.count
