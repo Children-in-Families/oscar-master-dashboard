6b561c92c6626a7412c96643f40502ef15867302
kirykr <kirymeas@gmail.com>
2020-10-19 16:50:10 +0700

    fixed save organization with referral source validation

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -22,7 +22,6 @@ class Organization < ApplicationRecord
   validates :supported_languages, presence: true
   validates :logo, presence: true
   validates :full_name, :short_name, presence: true
-  validates :referral_source_category_id, presence: :true
   validates :short_name, uniqueness: { case_sensitive: false }, format: { with: %r{\A[a-z](?:[a-z0-9-]*[a-z0-9])?\z}i }, length: { in: 1..63 }
 
   scope :demo, -> { where(demo: true) }
