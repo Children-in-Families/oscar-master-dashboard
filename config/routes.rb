b312e5961c74341fac844dd8e28c583ba5a8b80f
Vibol Teav <vibolteav@gmail.com>
2023-08-15 15:51:10 +0700

    init new UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
+  # devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "admin/dashboard#index"
+  
+  devise_for :admin_users
+  root to: "dashboards#show"
+  
+  resource :dashboard, only: :show do
+    resources :organizations
+    resources :admin_users
+  end
 end
