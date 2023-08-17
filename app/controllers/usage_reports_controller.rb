2926a29920d363cad45b0f6dd7f52ad839811769
Vibol Teav <vibolteav@gmail.com>
2023-08-17 15:57:06 +0700

    add usage report page

--- app/controllers/usage_reports_controller.rb
+++ app/controllers/usage_reports_controller.rb
@@ -1,4 +1,11 @@
 class UsageReportsController < ApplicationController
-  def index
+  def show
+    params[:q] ||= {}
+    params[:q][:month_eq] ||= 1.month.ago.month
+    params[:q][:year_eq] ||= Date.current.year
+    params[:q][:s] ||= "organizations.short_name ASC"
+ 
+    @q = UsageReport.ransack(params[:q])
+    @usage_reports = @q.result.includes(:organization)
   end
 end
---
+++ app/models/usage_report.rb
@@ -0,0 +1,3 @@
+class UsageReport < ActiveRecord::Base
+  belongs_to :organization
+end
