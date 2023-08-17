5ad0e7e7d30dde84b8f38f794e4dd437d0c11f71
Vibol Teav <vibolteav@gmail.com>
2023-08-17 17:30:23 +0700

    format value

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,18 @@ module ApplicationHelper
   def active_class(condition)
     condition ? 'active' : ''
   end
+
+  def format_value(value)
+    if value.is_a?(Date) || value.is_a?(Time)
+      value.strftime('%d/%m/%Y')
+    elsif value.is_a?(Array)
+      value.join(', ')
+    elsif value.is_a?(TrueClass)
+      'Yes'
+    elsif value.is_a?(FalseClass)
+      'No'
+    else
+      value
+    end
+  end
 end
