a50e4b5045eb47f71af31cc2967187baff938a92
Vibol Teav <vibolteav@gmail.com>
2023-08-24 16:19:15 +0700

    fix filter by date

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_admin_user!
+
+  before_action :fix_date_params
+
+  private
+
+  def fix_date_params
+    return unless params[:q]
+
+    params[:q].each do |key, value|
+      if key.ends_with?("_lteq") || key.ends_with?("_lt")
+        params[:q][key] = Date.parse(value).end_of_day if value.present?
+      end
+    end
+  end
 end
