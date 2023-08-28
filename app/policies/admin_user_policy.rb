c2f89a45a7479f293835920654fc02e76761c36b
Vibol Teav <vibolteav@gmail.com>
2023-08-28 18:24:39 +0700

    update authorization

--- app/policies/admin_user_policy.rb
+++ app/policies/admin_user_policy.rb
@@ -2,6 +2,13 @@ class AdminUserPolicy < ApplicationPolicy
   def destroy?
     user.admin?
   end
+
+  def edit?
+    user.admin? ||
+    (user.editor? && record.role.in?(%w[finance viewer]))
+  end
+
+  alias update? edit?
   
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
