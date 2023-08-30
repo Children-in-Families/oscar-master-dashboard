6436762db5f234318e52c97fbcbe2734e4532f8f
Vibol Teav <vibolteav@gmail.com>
2023-08-30 12:12:46 +0700

    filter by tenant

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,18 @@
 class MessagesController < ApplicationController
   def index
     authorize Ahoy::Message
-    
+
     params[:q] ||= {}
     params[:q][:s] ||= "created_at desc"
 
-    @q = Ahoy::Message.page(params[:page]).ransack(params[:q])
-    @messages = @q.result(distinct: true)
+    if params[:by_tenant]
+      Apartment::Tenant.switch(params[:by_tenant]) do
+        @q = Ahoy::Message.page(params[:page]).ransack(params[:q])
+        @messages = @q.result(distinct: true)
+      end
+    else
+      @q = Ahoy::Message.ransack(params[:q])
+      @messages = @q.result
+    end
   end
 end
