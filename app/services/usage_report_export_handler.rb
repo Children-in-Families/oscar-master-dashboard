c961399a612ed67735732f0ea5dac4c0bc310b7e
Vibol Teav <vibolteav@gmail.com>
2023-09-04 11:39:53 +0700

    fix syntax

--- app/services/usage_report_export_handler.rb
+++ app/services/usage_report_export_handler.rb
@@ -179,7 +179,7 @@ class UsageReportExportHandler
   def add_cases_sheet(package)
     headers = [
       'NGO Name',
-      '# Login'
+      '# Login',
       'Total client',
       'Adult female without disability',
       'Adult female with disability',
