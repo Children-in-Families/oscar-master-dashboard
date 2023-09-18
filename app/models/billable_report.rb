2b338e78538c9036e354a9b6ebfc668abece954a
Vibol Teav <vibolteav@gmail.com>
2023-09-18 14:52:12 +0700

    fix count

--- app/models/billable_report.rb
+++ app/models/billable_report.rb
@@ -6,6 +6,4 @@ class BillableReport < ActiveRecord::Base
   validates :month, presence: true, uniqueness: { scope: [:organization_id, :year] }
 
   has_many :billable_report_items
-  has_many :billable_clients, -> { joins(:version).where(versions: { item_type: 'Client' }) }, class_name: 'BillableReportItem'
-  has_many :billable_families, -> { joins(:version).where(version: { item_type: 'Family' }) }, class_name: 'BillableReportItem'
 end
