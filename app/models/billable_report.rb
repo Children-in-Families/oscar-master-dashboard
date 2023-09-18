a9c6a0deea141680a5afaffb66e1624e7395b66a
Vibol Teav <vibolteav@gmail.com>
2023-09-18 14:35:35 +0700

    fix class name

--- app/models/billable_report.rb
+++ app/models/billable_report.rb
@@ -6,6 +6,6 @@ class BillableReport < ActiveRecord::Base
   validates :month, presence: true, uniqueness: { scope: [:organization_id, :year] }
 
   has_many :billable_report_items
-  has_many :billable_clients, -> { joins(:version).where(versions: { item_type: 'Client' }) }
-  has_many :billable_families, -> { joins(:version).where(version: { item_type: 'Family' }) }
+  has_many :billable_clients, -> { joins(:version).where(versions: { item_type: 'Client' }) }, class_name: 'BillableReportItem'
+  has_many :billable_families, -> { joins(:version).where(version: { item_type: 'Family' }) }, class_name: 'BillableReportItem'
 end
