e4a44560ba4d731bb0bf8e23acdd572cfcecc8ef
Vibol Teav <vibolteav@gmail.com>
2023-09-26 09:54:59 +0700

    update duplicate page

--- app/controllers/duplications_controller.rb
+++ app/controllers/duplications_controller.rb
@@ -21,23 +21,19 @@ class DuplicationsController < ApplicationController
               'Duplicated Fields'
             ]
 
-            Organization.active.without_shared.each do |organization|
-              Organization.switch_to(organization.short_name)
-
-              Client.duplicate.find_each do |client|
-                sheet.add_row [
-                  client.slug,
-                  client.local_name,
-                  client.en_name,
-                  client.date_of_birth&.strftime('%Y-%m-%d'),
-                  client.gender,
-                  organization.full_name,
-                  client.created_at&.strftime('%Y-%m-%d'),
-                  client.duplicate_with['duplicated_with_ngo'],
-                  client.duplicate_with['duplicated_with_client_id'],
-                  client.duplicate_with['duplicate_fields'].join(', '),
-                ]
-              end
+            SharedClient.duplicate.find_each do |client|
+              sheet.add_row [
+                client.slug,
+                client.local_name,
+                client.en_name,
+                client.date_of_birth&.strftime('%Y-%m-%d'),
+                client.gender,
+                client.ngo_name,
+                client.client_created_at&.strftime('%Y-%m-%d'),
+                client.duplicate_with['duplicated_with_ngo'],
+                client.duplicate_with['duplicated_with_client_id'],
+                client.duplicate_with['duplicate_fields'].join(', '),
+              ]
             end
           end
           p.serialize(filename)
--- app/models/shared_client.rb
+++ app/models/shared_client.rb
@@ -1,4 +1,14 @@
 class SharedClient < ActiveRecord::Base
   self.table_name = 'shared.shared_clients'
   has_paper_trail
+
+  scope :duplicate, -> { where(duplicate: true) }
+
+  def local_name
+    [local_given_name, local_family_name].select(&:present?).join(' ')
+  end
+
+  def en_name
+    [given_name, family_name].select(&:present?).join(' ')
+  end
 end
