69828b82149bb240e674144b6e7f5f16fe744bdc
Vibol Teav <vibolteav@gmail.com>
2023-10-07 07:38:17 +0700

    servce static assets

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
@@ -63,9 +63,6 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "OSCaR_MD_production"
 
   config.action_mailer.perform_caching = false
-
-  config.action_controller.asset_host = "//#{ENV['S3_BUCKET_NAME']}.s3.amazonaws.com"
-  config.assets.prefix = "/assets"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
