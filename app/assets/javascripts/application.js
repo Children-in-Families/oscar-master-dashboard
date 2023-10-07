fdc65485942a347bd87ef5b44d3be5d0e5556322
Vibol Teav <vibolteav@gmail.com>
2023-10-07 09:04:10 +0700

    fix tiny mce

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,11 @@
+//= require tinymce
+
+$(document).on("turbolinks:load", function() {
+  tinymce.init({
+    selector: 'textarea.tinymce',
+    plugins: 'lists',
+    width : '100%',
+    toolbar: 'bold italic numlist bullist',
+    menubar: false
+  })
+})
