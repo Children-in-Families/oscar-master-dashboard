77a14c23c61adc4334263dc4fcbae05d197467bb
Vibol Teav <vibolteav@gmail.com>
2023-10-07 15:06:43 +0700

    fix tinymce when using with turbolinks

--- app/assets/javascripts/application.js
+++
@@ -1,9 +0,0 @@
-$(document).on("turbolinks:load", function() {
-  tinymce.init({
-    selector: 'textarea.tinymce',
-    plugins: 'lists',
-    width : '100%',
-    toolbar: 'bold italic numlist bullist',
-    menubar: false
-  })
-})
