adbf41e37c19842575145fefbceac39f49f8765f
Vibol Teav <vibolteav@gmail.com>
2023-10-07 15:22:22 +0700

    update toolbars for tinymce

--- app/policies/release_note_policy.rb
+++ app/policies/release_note_policy.rb
@@ -1,9 +1,12 @@
 class ReleaseNotePolicy < ApplicationPolicy
   def publish?
-    record.published_at.blank? && (user.admin? || user.editor?)
+    !record.published? && (user.admin? || user.editor?)
   end
 
   def new?
     user.admin? || user.editor?
   end
+
+  alias eidt? publish?
+  alias update? publish?
 end
