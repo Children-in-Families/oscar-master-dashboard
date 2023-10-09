0ec7a1509631b04e2df8d3f4704aec7c23e6d1df
Vibol Teav <vibolteav@gmail.com>
2023-10-10 05:53:41 +0700

    fix cannot find client/family

--- app/models/billable_report_item.rb
+++ app/models/billable_report_item.rb
@@ -1,6 +1,6 @@
 class BillableReportItem < ActiveRecord::Base
   belongs_to :billable_report
-  belongs_to :billable, polymorphic: true
+  belongs_to :billable, polymorphic: true, with_deleted: true
   belongs_to :version, class_name: "PaperTrail::Version"
 
   validates :billable_status, presence: true
