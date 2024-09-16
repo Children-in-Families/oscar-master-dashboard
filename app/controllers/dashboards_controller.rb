4f2c361ac74c46c9489fa50ba19cec279476241c
Vibol Teav <vibolteav@gmail.com>
2024-09-16 16:13:11 +0700

    WIP

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,5 +4,10 @@ class DashboardsController < ApplicationController
     Organization.switch_to('cif')
 
     @q = Client.with_deleted.reportable.ransack(params[:q])
+    @report_data = {
+      openning_case: @q.result.accepted_or_active.count,
+      closed_case: @q.result.exited.count,
+      risk_level: Assessment.client_risk_assessments.joins(:client).group(:level_of_risk).count
+    }
   end
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -23,4 +23,43 @@ class Client < ActiveRecord::Base
   scope :adult,                  ->        { where("(EXTRACT(year FROM age(current_date, clients.date_of_birth)) :: int) >= ?", 18) }
   scope :child,                  ->        { where("(EXTRACT(year FROM age(current_date, clients.date_of_birth)) :: int) < ?", 18) }
   scope :without_age_nor_gender, ->        { non_binary.where(date_of_birth: nil) }
+
+  scope :accepted_or_active,     ->        { where(status: %w(Accepted Active)) }
+  scope :exited,                 ->        { where(status: 'Exited') }
+
+  def self.client_based_aggregates
+    query = <<-SQL.squish
+      SELECT
+        COUNT(*) FILTER (WHERE status = 'Exited') AS exited,
+        COUNT(*) FILTER (WHERE status = 'Accepted') AS accepted,
+        COUNT(*) FILTER (WHERE status = 'Active') AS active,
+        COUNT(*) FILTER (WHERE status = 'Referred') AS referred,
+        COUNT(*) FILTER (WHERE gender = 'male') AS male_count,
+        COUNT(*) FILTER (WHERE gender = 'female') AS female_count,
+        COUNT(*) FILTER (WHERE gender NOT IN ('male', 'female')) as non_binary_count,
+        COUNT(*) FILTER (WHERE EXTRACT(YEAR FROM AGE(CURRENT_DATE, date_of_birth)) >= 18) AS age_18_plus,
+        COUNT(*) FILTER (WHERE EXTRACT(YEAR FROM AGE(CURRENT_DATE, date_of_birth)) BETWEEN 15 AND 17) AS age_15_17,
+        COUNT(*) FILTER (WHERE EXTRACT(YEAR FROM AGE(CURRENT_DATE, date_of_birth)) BETWEEN 10 AND 14) AS age_10_14,
+        COUNT(*) FILTER (WHERE EXTRACT(YEAR FROM AGE(CURRENT_DATE, date_of_birth)) BETWEEN 5 AND 9) AS age_5_9,
+        COUNT(*) FILTER (WHERE EXTRACT(YEAR FROM AGE(CURRENT_DATE, date_of_birth)) BETWEEN 0 AND 4) AS age_0_4,
+        COUNT(*) FILTER (WHERE date_of_birth IS NULL) AS no_dob
+      FROM clients;
+    SQL
+
+    ActiveRecord::Base.connection.execute(query).first
+  end
+
+  def self.client_risk_assessments_aggregates
+    query = <<-SQL.squish
+      SELECT
+        COUNT(*) FILTER (WHERE level_of_risk = 'low') AS risk_low,
+        COUNT(*) FILTER (WHERE level_of_risk = 'medium') AS risk_medium,
+        COUNT(*) FILTER (WHERE level_of_risk = 'high') AS risk_high,
+        COUNT(*) FILTER (WHERE level_of_risk = 'no action') AS risk_no_action
+      FROM clients
+      JOIN risk_assessments ON clients.id = risk_assessments.client_id;
+    SQL
+
+    ActiveRecord::Base.connection.execute(query).first
+  end
 end
